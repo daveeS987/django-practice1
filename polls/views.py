32c3dbce6e8b05e1a9db0c9d8f376808c6762743
Davee Sok <daveesok@gmail.com>
2021-08-25 11:53:42 -0500

    finished part3 views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,35 @@
-from django.http import HttpResponse
+from django import template
+from polls.models import Question
+from django.http import Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello World. This is the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"question": question})
+
+    # short version
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    context = {"question_id": question_id}
+    return render(request, "polls/results.html", context)
+
+
+def vote(request, question_id):
+    context = {"question_id": question_id}
+    return render(request, "polls/vote.html", context)
